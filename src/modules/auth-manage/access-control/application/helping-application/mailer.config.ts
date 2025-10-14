eddb971cd5cc218a67b240ad38d90299cb7fbb61
MaksLucky25 <maksrad93@gmail.com>
2025-10-14 17:31:57 +0300

    change mailer provider

--- src/modules/auth-manage/access-control/application/helping-application/mailer.config.ts
+++ src/modules/auth-manage/access-control/application/helping-application/mailer.config.ts
@@ -4,20 +4,13 @@ import { ConfigService } from '@nestjs/config';
 export const getMailerConfig = (
   configService: ConfigService,
 ): MailerOptions => {
-  const emailUser = configService.getOrThrow<string>('EMAIL_USER');
-
   return {
     transport: {
-      host: 'smtp.yandex.ru',
-      port: 587,
-      secure: false, // true для 465, false для других портов
+      service: 'gmail',
       auth: {
-        user: emailUser,
+        user: configService.getOrThrow<string>('EMAIL_USER'),
         pass: configService.getOrThrow<string>('EMAIL_PASS'),
       },
-    },
-    defaults: {
-      from: emailUser, // From должен совпадать с auth.user
     },
   };
 };
