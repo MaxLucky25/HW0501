ce013ac4e3963d076d7d34cb2a5f0344f97373fa
MaksLucky25 <maksrad93@gmail.com>
2025-10-14 20:03:54 +0300

    change email provider

--- src/modules/auth-manage/access-control/application/helping-application/email.service.ts
+++ src/modules/auth-manage/access-control/application/helping-application/email.service.ts
@@ -1,13 +1,18 @@
-import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import sgMail from '@sendgrid/mail';
 
 @Injectable()
 export class EmailService {
-  constructor(private mailerService: MailerService) {}
+  constructor(private configService: ConfigService) {
+    // Инициализируем SendGrid с API ключом (используем старые переменные)
+    sgMail.setApiKey(this.configService.getOrThrow<string>('EMAIL_PASS'));
+  }
 
   async sendConfirmationEmail(email: string, code: string): Promise<void> {
-    await this.mailerService.sendMail({
+    const msg = {
       to: email,
+      from: this.configService.getOrThrow<string>('EMAIL_USER'),
       subject: 'Подтверждение регистрации',
       text: `Подтвердите регистрацию по ссылке: https://somesite.com/confirm-email?code=${code}`,
       html: `
@@ -16,12 +21,15 @@ export class EmailService {
             <a href='https://somesite.com/confirm-email?code=${code}'>complete registration</a>
         </p>
       `,
-    });
+    };
+
+    await sgMail.send(msg);
   }
 
   async sendRecoveryEmail(email: string, recoveryCode: string): Promise<void> {
-    await this.mailerService.sendMail({
+    const msg = {
       to: email,
+      from: this.configService.getOrThrow<string>('EMAIL_USER'),
       subject: 'Восстановление пароля',
       text: `Восстановите пароль по ссылке: https://somesite.com/recover?code=${recoveryCode}`,
       html: `
@@ -30,6 +38,8 @@ export class EmailService {
             <a href='https://somesite.com/recover?code=${recoveryCode}'>reset password</a>
         </p>
       `,
-    });
+    };
+
+    await sgMail.send(msg);
   }
 }
--- src/modules/auth-manage/access-control/application/helping-application/helping-application.module.ts
+++ src/modules/auth-manage/access-control/application/helping-application/helping-application.module.ts
@@ -2,18 +2,10 @@ import { Module } from '@nestjs/common';
 import { BcryptService } from './bcrypt.service';
 import { EmailService } from './email.service';
 import { JwtConfigService } from './jwt-config.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { getMailerConfig } from './mailer.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule,
-    MailerModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: getMailerConfig,
-    }),
-  ],
+  imports: [ConfigModule],
   providers: [BcryptService, EmailService, JwtConfigService],
   exports: [BcryptService, EmailService, JwtConfigService],
 })
